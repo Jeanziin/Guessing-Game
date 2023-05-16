27ddda6fe22d3568a4cea37ccd1b2830b4999907
Jeanziin <jjeanfreitass@gmail.com>
2023-05-16 11:03:27 -0300

    practicing

---
+++ guessing-game/public/main.js
@@ -0,0 +1,34 @@
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+function handleClick(event)
+{
+    event.preventDefault();
+
+    const inputNumber = document.querySelector("#inputNumber");
+    
+    if(Number(inputNumber.value == randomNumber)) {
+        document.querySelector(".screen1").classList.add("hide");
+        document.querySelector(".screen2").classList.remove("hide");
+   
+        document
+        .querySelector(".screen2 h2")
+        .innerText = `Got it right in ${xAttempts} Attempts!!`;
+    }
+
+    xAttempts++;
+    
+}
+
+function againClick(event)
+{
+    event.preventDefault();
+    const buttonNumber = document.querySelector("#buttonNumber");
+    if(buttonNumber) 
+    {
+        document.querySelector(".screen1").classList.remove("hide");
+        document.querySelector(".screen2").classList.add("hide");
+    }
+    
+
+}
