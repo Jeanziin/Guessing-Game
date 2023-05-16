5a237731a096b612ff7611e8c8f8eb738563655a
Jeanziin <jjeanfreitass@gmail.com>
2023-05-16 15:38:14 -0300

    refactoring the code

--- guessing-game/public/main.js
+++ guessing-game/public/main.js
@@ -1,34 +1,49 @@
-const randomNumber = Math.round(Math.random() * 10);
-let xAttempts = 1;
-
-function handleTryClick(event)
-{
-    event.preventDefault();
-
-    const inputNumber = document.querySelector("#inputNumber");
-    
-    if(Number(inputNumber.value == randomNumber)) {
-        document.querySelector(".screen1").classList.add("hide");
-        document.querySelector(".screen2").classList.remove("hide");
-   
-        document
-        .querySelector(".screen2 h2")
-        .innerText = `Got it right in ${xAttempts} Attempts!`;
-    }
-
-    inputNumber.value = "";
-    xAttempts++;
-    
-}
-
-//Eventos
-
 const btnTry = document.querySelector("#btnTry");
 const btnReset = document.querySelector("#btnReset");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const resultText = screen2.querySelector("h2");
 
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click',  ()=> {
-    document.querySelector(".screen1").classList.remove("hide");
-    document.querySelector(".screen2").classList.add("hide");
-    xAttempts = 1;
-})
+let xAttempts = 0;
+let randomNumber;
+
+//Events
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+
+//Functions 
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 11);
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+function updateResultText() {
+  resultText.innerText = `Got it right in ${xAttempts} Attempt${xAttempts !== 1 ? 's' : ''}!`;
+}
+
+function handleTryClick(event) {
+  event.preventDefault();
+
+  const inputNumber = Number(document.querySelector("#inputNumber").value);
+
+  if (inputNumber === randomNumber) {
+    updateResultText();
+    toggleScreen();
+  }
+
+  document.querySelector("#inputNumber").value = "";
+  xAttempts++;
+}
+
+function handleResetClick() {
+  toggleScreen();
+  xAttempts = 0;
+  randomNumber = generateRandomNumber();
+}
+
+// Initial setup
+randomNumber = generateRandomNumber();
